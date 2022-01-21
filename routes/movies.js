0d746b69b840244b1ad636c4f764619611372930
ELYOUSFI Mohamed <73071155+ElyousfiMohamed@users.noreply.github.com>
2022-01-21 12:30:22 +0100

    D - ACCES AUX DONNEES

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,9 @@
-var express = require('express');
-var movies = require('../movies_reduit.json');
-var router = express.Router();
-
-/* GET movies listing. */
-router.get('/', function(req, res, next) {
-  res.send(movies);
+const express = require('express');
+const router = express.Router();
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+router.get('/', async function(req, res, next) {
+const movies = await prisma.movies.findMany({take: 10})
+res.send(movies);
 });
-
 module.exports = router;
