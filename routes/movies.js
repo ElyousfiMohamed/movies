52c71e3a1c4dfd05e510acf6fdfe8e488ddfa1f3
ELYOUSFI Mohamed <73071155+ElyousfiMohamed@users.noreply.github.com>
2022-01-22 10:04:34 +0100

    E - FRONTEND SVELTE

--- routes/movies.js
+++ routes/movies.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {PrismaClient} = require('@prisma/client')
-const prisma = new PrismaClient()
-router.get('/', async function(req, res, next) {
-const movies = await prisma.movies.findMany({take: 10})
-res.send(movies);
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+router.get("/:take?/:skip?", async function (req, res, next) {
+  const limit = Number.parseInt(req.query.take) || 10;
+  const offset = Number.parseInt(req.query.skip) || 0;
+  const movies = await prisma.movies.findMany({
+    take: limit,
+    skip: offset,
+  });
+  res.send({
+    data: movies,
+    pagination: {
+      count: movies.length,
+      take: limit,
+      skip: offset,
+    },
+  });
 });
 module.exports = router;
